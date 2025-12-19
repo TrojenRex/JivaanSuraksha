1fcf7db838a7c06af20be84e45e875150e357764
Abhradeep Basu <officialabhradeep@gmail.com>
2025-12-19 20:15:08 +0000

    add more layers of shade for background

--- src/components/wave-background.tsx
+++ src/components/wave-background.tsx
@@ -6,6 +6,8 @@ const WaveBackground: FC = () => {
       <div className="wave wave1"></div>
       <div className="wave wave2"></div>
       <div className="wave wave3"></div>
+      <div className="wave wave4"></div>
+      <div className="wave wave5"></div>
     </div>
   );
 };
