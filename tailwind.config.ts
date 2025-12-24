a894f75bea944f817e8e2aca755eb2931fcb2255
Abhradeep Basu <officialabhradeep@gmail.com>
2025-12-24 19:47:55 +0000

    round up the edges of the buttons and sections

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -74,9 +74,10 @@ export default {
         },
       },
       borderRadius: {
-        lg: 'var(--radius)',
-        md: 'calc(var(--radius) - 2px)',
-        sm: 'calc(var(--radius) - 4px)',
+        xl: `calc(var(--radius) + 4px)`,
+        lg: `var(--radius)`,
+        md: `calc(var(--radius) - 4px)`,
+        sm: 'calc(var(--radius) - 8px)',
       },
       keyframes: {
         'accordion-down': {
