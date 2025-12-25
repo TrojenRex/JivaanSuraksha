8b0018d4cc5b7059eed4ed1b8b330759d7309bae
Abhradeep Basu <officialabhradeep@gmail.com>
2025-12-25 08:10:46 +0000

    extend the wave to the bottom

--- src/components/wave-background.tsx
+++ src/components/wave-background.tsx
@@ -5,14 +5,14 @@ const WaveBackground = () => {
         className="waves"
         xmlns="http://www.w3.org/2000/svg"
         xmlnsXlink="http://www.w3.org/1999/xlink"
-        viewBox="0 24 150 28"
+        viewBox="0 24 150 40"
         preserveAspectRatio="none"
         shapeRendering="auto"
       >
         <defs>
           <path
             id="gentle-wave"
-            d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z"
+            d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v60h-352z"
           />
         </defs>
         <g className="parallax">
